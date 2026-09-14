c46740aa3580b39b7194d203fa1042590bbf37ab
hongdaeun11 <sally20114@gmail.com>
2026-09-14 15:06:10 +0900

    09/14 db 2차 수정

--- anyangproj_ai_analyses.sql
+++ anyangproj_ai_analyses.sql
@@ -35,7 +35,7 @@ CREATE TABLE `ai_analyses` (
   KEY `fk_ai_analysis_report_image` (`report_image_id`),
   CONSTRAINT `fk_ai_analyses_report` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_ai_analysis_report_image` FOREIGN KEY (`report_image_id`) REFERENCES `report_images` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `ai_analyses` (
 
 LOCK TABLES `ai_analyses` WRITE;
 /*!40000 ALTER TABLE `ai_analyses` DISABLE KEYS */;
-INSERT INTO `ai_analyses` VALUES (18,18,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/aa32c94a-e333-4ce1-a491-716a9610c0b2.jpg','2026-09-12 18:27:50',20);
+INSERT INTO `ai_analyses` VALUES (18,18,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/aa32c94a-e333-4ce1-a491-716a9610c0b2.jpg','2026-09-12 18:27:50',20),(19,21,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/c4ecdda4-bd3a-45e5-8d74-7e41213b2351.jpg','2026-09-14 12:04:05',23),(20,22,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/e34457cd-cdd9-41ed-a233-c5d9a7010f34.jpg','2026-09-14 12:12:16',24),(21,23,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/7e05fae8-bc6d-4b4b-85d0-1ccb3598c956.jpg','2026-09-14 12:20:26',25),(22,24,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/00df8f2f-fa6e-4fb8-a9ab-d6644f7cccf5.jpg','2026-09-14 12:22:36',26),(23,25,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/1fb50c60-0cb3-4498-950b-3392cf2f8976.jpg','2026-09-14 12:24:01',27),(24,26,'yolov8s','rdd2022','https://anyang-project-bucket.s3.amazonaws.com/6cbc6326-ae4d-4091-9462-e26c0decc641.jpg','2026-09-14 13:08:25',28);
 /*!40000 ALTER TABLE `ai_analyses` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-12 23:16:44
+-- Dump completed on 2026-09-14 13:58:57
--- anyangproj_report_images.sql
+++ anyangproj_report_images.sql
@@ -31,7 +31,7 @@ CREATE TABLE `report_images` (
   PRIMARY KEY (`id`),
   KEY `fk_report_images_report` (`report_id`),
   CONSTRAINT `fk_report_images_report` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `report_images` (
 
 LOCK TABLES `report_images` WRITE;
 /*!40000 ALTER TABLE `report_images` DISABLE KEYS */;
-INSERT INTO `report_images` VALUES (20,18,'https://anyang-project-bucket.s3.amazonaws.com/d77444d3-47c1-4448-ae99-1de431882e8c.png','image/jpeg','2026-09-12 18:27:46');
+INSERT INTO `report_images` VALUES (20,18,'https://anyang-project-bucket.s3.amazonaws.com/d77444d3-47c1-4448-ae99-1de431882e8c.png','image/jpeg','2026-09-12 18:27:46'),(21,19,'https://anyang-project-bucket.s3.amazonaws.com/f3fab19f-5b7e-4712-b025-72ba2b54c6e1.png','image/jpeg','2026-09-14 11:26:58'),(23,21,'https://anyang-project-bucket.s3.amazonaws.com/d8ab090e-a4f1-4c62-a383-085bbe0ab0c5.png','image/jpeg','2026-09-14 12:03:57'),(24,22,'https://anyang-project-bucket.s3.amazonaws.com/db72e45b-d8df-4080-b2fe-3a4a5e28c03c.png','image/png','2026-09-14 12:12:13'),(25,23,'https://anyang-project-bucket.s3.amazonaws.com/1376c051-d844-41f0-8399-ec7ef05f7fa4.png','image/png','2026-09-14 12:20:25'),(26,24,'https://anyang-project-bucket.s3.amazonaws.com/71f10185-f203-45d4-ac04-56a1da0ec20d.png','image/jpeg','2026-09-14 12:22:36'),(27,25,'https://anyang-project-bucket.s3.amazonaws.com/d54babb6-6e3a-49aa-9e60-4180334747b3.png','image/png','2026-09-14 12:24:00'),(28,26,'https://anyang-project-bucket.s3.amazonaws.com/68dc0552-8fe7-4268-919c-581790c61b7c.png','image/png','2026-09-14 13:08:24');
 /*!40000 ALTER TABLE `report_images` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-12 23:16:43
+-- Dump completed on 2026-09-14 13:58:56
